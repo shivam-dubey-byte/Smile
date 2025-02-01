7a7d5179867f62ece0fbe246adf50a125045ae1c
Shivam Raj Dubey <dubeyshivamraj@gmail.com>
2025-02-02 04:53:38 +0530

    0.1.1

--- SMILE/api/server.py
+++ SMILE/api/server.py
@@ -1,15 +1,19 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 from deepface import DeepFace
 from PIL import Image
+import tempfile
 import io
 
 app=Flask(__name__)
 
 def get_happiness_score(image):
-    image = Image.open(io.BytesIO(image))
-    analysis = DeepFace.analyze(img_path=image, actions=['emotion'], enforce_detection=False)
+    #image = Image.open(io.BytesIO(image))
+    with tempfile.NamedTemporaryFile(delete=False, suffix=".jpg") as temp_file:
+        image.save(temp_file, "JPEG")
+        temp_file_path = temp_file.name
+    analysis = DeepFace.analyze(img_path=temp_file_path, actions=['emotion'])
     emotion_data = analysis[0]['emotion']
-    smile_score = emotion_data.get('happy', 0.0)
+    smile_score = emotion_data['happy'] #.get('happy', 0.0)
     return float(smile_score)
 
 
@@ -21,9 +25,11 @@ def AboutUs():
 def predict():
     if 'image' not in request.files:
         return jsonify({'error': 'No image file provided'}), 400
+    
+    image_data = Image.open(request.files['image'])
 
-    image_file = request.files['image']
-    image_data = image_file.read()
+    #image_file = request.files['image']
+    #image_data = image_file.read()
     try:
         score = get_happiness_score(image_data)
         return jsonify({'happiness_score': score})
