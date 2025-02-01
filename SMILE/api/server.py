3080be31950ecc52ca88030cc054b479943e5b09
Shivam Raj Dubey <dubeyshivamraj@gmail.com>
2025-02-02 02:25:09 +0530

    0.0.9

--- SMILE/api/server.py
+++ SMILE/api/server.py
@@ -5,6 +5,14 @@ import io
 
 app=Flask(__name__)
 
+def get_happiness_score(image):
+    image = Image.open(io.BytesIO(image))
+    analysis = DeepFace.analyze(img_path=image, actions=['emotion'], enforce_detection=False)
+    emotion_data = analysis[0]['emotion']
+    smile_score = emotion_data.get('happy', 0.0)
+    return float(smile_score)
+
+
 @app.route("/")
 def AboutUs():
     return "Smile ML Working!! "#render_template('AboutUs.html',home='',about='active',donate="",contact="")
